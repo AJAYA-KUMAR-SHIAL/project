4b87b5de159353f0418e4404246cb6fa274af680
Ajaya kumar <168890540+AJAYA-KUMAR-SHIAL@users.noreply.github.com>
2024-05-04 11:39:11 +0530

    first
    good

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="hidden md:block">
+      <h1 className="text-3xl font-bold  md:text-9xl md:text-center ">
+      Hello Ajaya
+    </h1>
+    </div>
+  )
+}
